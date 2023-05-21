77e070c8aca2a518165a4cb73b0bc9b88c735193
Niko Heikkilä <niko.heikkila@futurice.com>
2023-05-21 20:47:35 +0300

    feat: add treesitter manual trigger

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,17 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
       ensure_installed = {
         "bash",
         "html",
