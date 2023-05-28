9273646cc2c6565fbf23f99ec3bcf64e18c9b3c6
Niko Heikkilä <niko.heikkila@futurice.com>
2023-05-28 21:29:41 +0300

    feat: add shortcut for opening weekly journal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local zen = require("zen-mode")
+
 local function map(mode, lhs, rhs, opts)
   local keys = require("lazy.core.handler").handlers.keys
   ---@cast keys LazyKeysHandler
@@ -16,3 +18,12 @@ end
 map("n", "<leader>uz", "<cmd>ZenMode<cr>", { desc = "Toggle Zen mode" })
 map({ "n", "v", "i" }, "<M-Up>", "<cmd>:m -2<cr>", { desc = "Move line up" })
 map({ "n", "v", "i" }, "<M-Down>", "<cmd>:m +1<cr>", { desc = "Move line down" })
+
+map("n", "<leader>j", function()
+  local filepath = os.getenv("HOME") .. "/Dropbox/Notes/Journal"
+  local date = os.date("%W")
+  local filename = filepath .. "/Week-" .. date .. ".md"
+
+  vim.cmd("tabnew " .. filename)
+  zen.toggle()
+end, { desc = "Open weekly journal", silent = true })
