f90b6d93fccf9299d418e1aa1368282df681efe3
Niko Heikkilä <niko.heikkila@futurice.com>
2023-05-28 11:32:11 +0300

    fix: make the terminal background color same as editor

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,6 +4,8 @@ return {
     opts = {
       size = 15,
       open_mapping = [[<c-a-t>]],
+      -- Make the terminal background same as the editor background
+      shading_factor = 0,
     },
   },
 }
