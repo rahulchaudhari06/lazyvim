bc7f7f283e2134cf6f044e2b23a546eeb4503f2d
Niko Heikkilä <niko.heikkila@futurice.com>
2023-05-28 14:11:18 +0300

    feat: add Neotest plugin for unit tests

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.test.core" },
     { import = "plugins" },
   },
   defaults = {
