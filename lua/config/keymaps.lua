c0e4184cc7b3a1a412b0973e84c6a54cf1e5ee88
Rahul Chaudhari_ <rahulgc545@gmail.com>
2024-09-02 15:07:16 +0530

    first commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,6 +19,26 @@ map("n", "<leader>uz", "<cmd>ZenMode<cr>", { desc = "Toggle Zen mode" })
 map({ "n", "v", "i" }, "<M-Up>", "<cmd>:m -2<cr>", { desc = "Move line up" })
 map({ "n", "v", "i" }, "<M-Down>", "<cmd>:m +1<cr>", { desc = "Move line down" })
 
+-- Move to start/end of line
+map("n", "<D-Left>", "^", { desc = "Move to the beginning of the line" })
+map("n", "<D-Right>", "$", { desc = "Move to the end of the line" })
+
+-- Jump between words
+
+map("n", "<A-Left>", "b", { desc = "Jump to the previous word" })
+map("n", "<A-Right>", "w", { desc = "Jump to the next word" })
+
+-- Select text to next/previous word
+map("v", "<D-Right>", "veiw", { desc = "Select to the next word" })
+map("v", "<D-Left>", "b", { desc = "Select to the previous word" })
+
+-- Delete whole words
+map("n", "<D-Delete>", "dw", { desc = "Delete the word under the cursor" })
+-- Delete whole lines
+map("v", "<D-Delete>", "daw", { desc = "Delete the word under the cursor" })
+
+map("n", "<D-Backspace>", "dd", { desc = "Delete the current line" })
+
 map("n", "<leader>j", function()
   local filepath = os.getenv("HOME") .. "/Dropbox/Notes/Journal"
   local date = os.date("%W")
